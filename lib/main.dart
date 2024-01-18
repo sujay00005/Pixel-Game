0f3ee5eaf15272c52e7447af6f394de3c086f9fb
sujay-lite <sujay@hibuscushealth.com>
2024-01-18 20:48:43 +0530

    initialization change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-import 'game/flappy_bird_game.dart';
-
 void main() {
   final game = FlameGame();
   runApp(GameWidget(game: game));
